1cc894d7744e8e2aaa58e0212709a092e908a8f5
dante0527 <19dante.j.lee@gmail.com>
2023-12-03 03:18:37 -0500

    configured for project 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://ysbme1x5q4.execute-api.us-east-1.amazonaws.com'
 };
